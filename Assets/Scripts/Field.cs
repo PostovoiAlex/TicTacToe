986b51efa24a4012251e10595d7c8753148259d0
Roman Bukhtiiarov <roman.bukhtiiarov@unity3d.com>
2023-12-27 10:14:25 -0500

    Example for tic tac toe project. No win conditions yet

--- Assets/Scripts/Field.cs
+++ Assets/Scripts/Field.cs
@@ -1,25 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
-public class Field : MonoBehaviour
+namespace TicTacToe
 {
-    [SerializeField] GameObject _tilePrefab;
-    [SerializeField] int _dimentions;
-
-    [ContextMenu("Awake")]
-    private void Awake()
+    public class Field : MonoBehaviour
     {
-        for (int i = 0; i < _dimentions; i++)
+        [SerializeField] GameObject _tilePrefab;
+        [SerializeField] int _dimentions;
+
+        Tile _lastSelectedTile;
+        public Tile LastSelectedTile => _lastSelectedTile;
+        
+        public Action OnTileSelected;
+        
+        [ContextMenu("Init")]
+        public void Init()
         {
-            for (int j =0; j < _dimentions; j++)
+            for (int i = 0; i < _dimentions; i++)
             {
-                GameObject copy = Instantiate(_tilePrefab);
-                copy.transform.SetParent(transform);
-                //copy.transform.localScale = Vector3.one; //сгенерував через чат gpt
-                copy.transform.position = new Vector3(200 * i, 200 * j, 0);
+                for (int j = 0; j < _dimentions; j++)
+                {
+                    Tile copy = Instantiate(_tilePrefab, transform).GetComponent<Tile>();
+                    copy.Init();
+                    copy.OnTileClicked += OnTileClicked;
+                    copy.transform.localPosition = new Vector3(200 * i, 200 * j, 0);
+                }
             }
         }
 
+        public void SetTileSprite(Tile tile, Sprite sprite)
+        {
+            tile.SetSprite(sprite);
+        }
+        
+        private void OnTileClicked(Tile tile)
+        {
+            if (tile.State != TileState.Empty)
+            {
+                return;
+            }
+
+            _lastSelectedTile = tile;
+            _lastSelectedTile.SetState(TileState.Filled);
+            OnTileSelected?.Invoke();
+        }
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,68 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+namespace TicTacToe
 {
-    [SerializeField] Player[] player;
+    public class GameManager : MonoBehaviour
+    {
+        [SerializeField] Player[] players;
+        Player _currentPlayer;
+        [SerializeField] Field _field;
+        [SerializeField] Turn _turn;
+
+        void Awake()
+        {
+            _field.Init();
+            _field.OnTileSelected += OnTileSelected;
+            
+            _turn.Init(_field);
+            
+            _turn.OnTurnStarted += OnTurnStarted;
+            _turn.OnTurnEnded += OnTurnEnded;
+        }
+        void OnTileSelected()
+        {
+            _field.SetTileSprite(_field.LastSelectedTile, _currentPlayer.PlayerTileSprite);
+        }
+
+        public void Start()
+        {
+            StartTurn();
+        }
+
+        void OnTurnEnded()
+        {
+            //TODO: Check win condition
+            StartTurn();
+        }
+        
+        void OnTurnStarted()
+        {
+            //throw new System.NotImplementedException();
+        }
+
+        void StartTurn()
+        {
+            Player nextPlayer = GetNextPlayer();
+
+            _currentPlayer = nextPlayer;
+            
+            _currentPlayer.StartTurn();
+            _turn.StartTurn();
+        }
+        
+        Player GetNextPlayer()
+        {
+            int nextPlayerIndex = 0;
+            for (int i = 0; i < players.Length; i++)
+            {
+                if (players[i] == _currentPlayer)
+                {
+                    nextPlayerIndex = (++i) < players.Length ? i : 0;
+                    break;
+                }
+            }
+
+            return players[nextPlayerIndex];
+        }
+    }
 }
